80ec77114fb06c0de208049493ffbba2c07513fd
Eric Fei <guanwha@gmail.com>
2020-01-02 16:58:18 +0800

    Adjusted codes

--- scripts/all.js
+++ scripts/all.js
@@ -1,6 +1,8 @@
-// functions
-let getScenesInZone = () => {
-  let records = responseData.result.records;
+/// functions ///
+
+// get scenes from response text
+let getScenesInZone = (responseText) => {
+  let records = responseText.result.records;
   let scenesInZone = {};
   // each record loop
   for (let i=0; i<records.length; i++) {
@@ -19,19 +21,18 @@ let getScenesInZone = () => {
 };
 
 
-// main code
+/// main code ///
+
+// get the data from Internet
 let xhr = new XMLHttpRequest();
 xhr.open('get',
          'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97',
          false);    // wait for data
-console.log('xhr.open()');
-
 xhr.send();
-console.log('xhr.send()');
 
-let responseData = JSON.parse(xhr.responseText);  // trace json data
-
-let scenesInZone = getScenesInZone();             // analysis scenes in every zone
+// analysis data
+let responseText = JSON.parse(xhr.responseText);              // trace json data
+let scenesInZone = getScenesInZone(responseText);             // analysis scenes in every zone
 console.log(Object.keys(scenesInZone));
 
 
