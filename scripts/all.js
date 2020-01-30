9c744e7e0a1c0ccc93974a32e5772ffc27666721
Eric Fei <guanwha@gmail.com>
2020-01-30 19:46:20 +0800

    Implemented the template layout of scene card
    1. add genSceneCard() to create template layout
    2. remove redundancy css

--- scripts/all.js
+++ scripts/all.js
@@ -50,12 +50,98 @@ let updateSceneList = () => {
   // update the scene cards in the list
   let elSceneListContent = document.querySelector('#scene-list');
   elSceneListContent.innerHTML = '';
-  let subEl = '<li class="scene-card"></li>';
-  let html = '';
   for (let i=0; i<scenesInCurZone.length; i++) {
-    html += subEl;
+    elSceneListContent.appendChild(genSceneCard(scenesInCurZone[i]));
   }
-  elSceneListContent.innerHTML = html;
+}
+
+// scene card
+let genSceneCard = (scene) => {
+  // <li class="scene-card">
+  //   <div class="card-top">
+  //     <div class="bg"></div>
+  //     <div class="name">高雄願景館</div>
+  //     <div class="zone">三民區</div>
+  //   </div>
+  //   <div class="card-bottom flex-ccl">
+  //     <div class="info flex-rlc"><span class="icon-opentime"></span> 週二至週日10:00-18:00，每週一公休</div>
+  //     <div class="info flex-rlc"><span class="icon-add"></span> 高雄市三民區建國二路318號</div>
+  //     <div class="info flex-rlc"><span class="icon-tel"></span> 886-7-2363357</div>
+  //     <div class="tag flex-rlc"><span class="icon-tag"></span> 免費參觀</div>
+  //   </div>
+  // </li>
+
+  let el = document.createElement('li');
+  el.setAttribute('class', 'scene-card');
+
+  // top part of card
+  let elTop = document.createElement('div');
+  elTop.setAttribute('class', 'card-top');
+  el.appendChild(elTop);
+
+  let elPicture = document.createElement('div');
+  elPicture.setAttribute('class', 'bg');
+  elPicture.setAttribute('style', `background-image: url(${scene.Picture1})`);
+  elTop.appendChild(elPicture);
+
+  let elName = document.createElement('div');
+  elName.setAttribute('class', 'name');
+  elName.textContent = scene.Name;
+  elTop.appendChild(elName);
+
+  let elZone = document.createElement('div');
+  elZone.setAttribute('class', 'zone');
+  elZone.textContent = scene.Zone;
+  elTop.appendChild(elZone);
+
+  // bottom part of card
+  let elBottom = document.createElement('div');
+  elBottom.setAttribute('class', 'card-bottom flex-ccl');
+  el.appendChild(elBottom);
+
+  let elOpenTime = document.createElement('div');
+  elOpenTime.setAttribute('class', 'info flex-rlc');
+  let elOpenTimeIcon = document.createElement('span');
+  elOpenTimeIcon.setAttribute('class', 'icon-opentime');
+  elOpenTime.appendChild(elOpenTimeIcon);
+  let elOpenTimeText = document.createElement('span');
+  elOpenTimeText.textContent = scene.Opentime;
+  elOpenTime.appendChild(elOpenTimeText);
+  elBottom.appendChild(elOpenTime);
+
+  let elAdd = document.createElement('div');
+  elAdd.setAttribute('class', 'info flex-rlc');
+  let elAddIcon = document.createElement('span');
+  elAddIcon.setAttribute('class', 'icon-add');
+  elAdd.appendChild(elAddIcon);
+  let elAddText = document.createElement('span');
+  elAddText.textContent = scene.Add;
+  elAdd.appendChild(elAddText);
+  elBottom.appendChild(elAdd);
+
+  let elTel = document.createElement('div');
+  elTel.setAttribute('class', 'info flex-rlc');
+  let elTelIcon = document.createElement('span');
+  elTelIcon.setAttribute('class', 'icon-tel');
+  elTel.appendChild(elTelIcon);
+  let elTelText = document.createElement('span');
+  elTelText.textContent = scene.Tel;
+  elTel.appendChild(elTelText);
+  elBottom.appendChild(elTel);
+
+  if (scene.Ticketinfo) {
+    let elTag = document.createElement('div');
+    elTag.setAttribute('class', 'tag flex-rlc');
+    let elTagIcon = document.createElement('span');
+    elTagIcon.setAttribute('class', 'icon-tag');
+    elTag.appendChild(elTagIcon);
+    let elTagText = document.createElement('span');
+    elTagText.textContent = scene.Ticketinfo;
+    elTag.appendChild(elTagText);
+    elBottom.appendChild(elTag);
+  }
+
+  return el;
 }
 
 
@@ -84,7 +170,7 @@ if (domSelectZone) {
     currentZoneName = domSelectZone.value;
     scenesInCurZone = scenesInAllZones[currentZoneName];
     // update the scene list
-    // updateSceneList();
+    updateSceneList();
   });
 }
 
